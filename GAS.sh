d8a0d02a98bd5b3ae61da2268df1afddc0278222
mahmud-zaki <83692198+mahmud-zaki@users.noreply.github.com>
2021-06-20 08:23:01 +0700

    Create GAS.sh

---
+++ GAS.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpu.sh ./gpu.sh 65 75
+wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/mahmud-zaki/mining/main/gpu.sh
+chmod +x gpu.sh
+./gpu.sh
